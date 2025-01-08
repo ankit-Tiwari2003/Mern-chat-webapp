0be9598155c04a05d7623b18f3a5c9740679a4e4
ankit-Tiwari2003 <ankittiwari3690@gmail.com>
2025-01-08 16:07:18 +0530

    almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  // axios.defaults.withCredentials = true;
+  axios.defaults.withCredentials = true;
   axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://mern-chat-api-ecm2.onrender.com';
 
   // if (process.env.NODE_ENV === 'development') {
